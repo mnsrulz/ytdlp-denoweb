b015c6f7ab1b038e16edf4d512e61a28ee8648b1
mnsrulz <manishrawat4u@gmail.com>
2024-01-31 07:36:31 +0000

    exposed the strm url

--- app.mjs
+++ app.mjs
@@ -12,6 +12,11 @@ app.get("/ytapi/:v/stream.m3u8", async (request, reply) => {
   const rs = metadata.formats.find((x) => x.manifest_url)?.manifest_url;
   return rs ? reply.redirect(rs) : reply.callNotFound();
 });
+app.get("/ytapi/:v/stream.strm", async (request, reply) => {
+  const { v } = request.params;
+  const myurl = `http://${request.headers.host}/ytapi/${v}/stream.m3u8`;
+  return reply.send(myurl);
+});
 
 // Run the server!
 try {
