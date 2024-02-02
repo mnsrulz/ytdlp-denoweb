bc89583a76c9720145c7cb6439f03950c43ea518
mnsrulz <manishrawat4u@gmail.com>
2024-02-02 04:16:01 +0000

    reading from cache

--- app.mjs
+++ app.mjs
@@ -2,6 +2,11 @@ import Fastify from "fastify";
 import ytdlp from "youtube-dl-exec";
 import HLS from 'hls-parser';
 const { MasterPlaylist } = HLS.types;
+import NodeCache from "node-cache";
+
+const myCache = new NodeCache({
+  stdTTL: process.env.DEFAULT_CACHE_TTL || 3 * 60 * 60 //3 hours
+});
 
 const app = Fastify({
   logger: true,
@@ -22,17 +27,20 @@ app.get("/ytapi/:v/stream.strm", async (request, reply) => {
 
 app.get("/ytapi/:v/beststream.m3u8", async (request, reply) => {
   const { v } = request.params;
-  const yturl = `https://www.youtube.com/v/${v}`;
-  const metadata = await ytdlp(yturl, { dumpSingleJson: true });
-  const manifestUrl = metadata.formats.find((x) => x.manifest_url)?.manifest_url;
-  const manifestData = await fetch(manifestUrl).then(x => x.text());
-  const parsedHls = HLS.parse(manifestData);
-  // var bestVp9Variant = parsedHls.variants.filter(x => x.codecs.startsWith('vp09')).sort((a, b) => b.bandwidth - a.bandwidth)[0];
-  // bestVp9Variant.subtitles = []
-  var bestAvcVariant = parsedHls.variants.filter(x => x.codecs.startsWith('avc')).sort((a, b) => b.bandwidth - a.bandwidth)[0];
-  bestAvcVariant.subtitles = []
-  const minimalPlaylist = new MasterPlaylist({ variants: [bestAvcVariant] });
-  const streamData = HLS.stringify(minimalPlaylist);
+  let streamData = myCache.get(v);
+  if (!streamData) {
+    const yturl = `https://www.youtube.com/v/${v}`;
+    const metadata = await ytdlp(yturl, { dumpSingleJson: true });
+    const manifestUrl = metadata.formats.find((x) => x.manifest_url)?.manifest_url;
+    const manifestData = await fetch(manifestUrl).then(x => x.text());
+    const parsedHls = HLS.parse(manifestData);
+    // var bestVp9Variant = parsedHls.variants.filter(x => x.codecs.startsWith('vp09')).sort((a, b) => b.bandwidth - a.bandwidth)[0];
+    // bestVp9Variant.subtitles = []
+    var bestAvcVariant = parsedHls.variants.filter(x => x.codecs.startsWith('avc')).sort((a, b) => b.bandwidth - a.bandwidth)[0];
+    bestAvcVariant.subtitles = []
+    const minimalPlaylist = new MasterPlaylist({ variants: [bestAvcVariant] });
+    streamData = HLS.stringify(minimalPlaylist);
+  }
   return reply.send(streamData);
 });
 
